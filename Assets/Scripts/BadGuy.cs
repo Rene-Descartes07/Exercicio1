30a32fda3350c8012643e053ef0193e607009b4d
Rene Guimaraes <reneguimaraes2015@gmail.com>
2018-09-29 17:13:04 -0300

    "Mais teste do que nunca"

---
+++ Assets/Scripts/BadGuy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System; 
+
+public class BadGuy : IComparable<BadGuy>
+{
+    [Header("Dados do Jogador")]
+    [HideInInspector] public string name;
+    [Range (0,100)] public int level;
+    [SerializeField] string story;
+
+    public BadGuy(string newName, int newlevel)
+    {
+        name = newName;
+        level = newlevel;
+    }
+
+    public int CompareTo(BadGuy other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        return level - other.level;
+    }
+}
---
+++ Assets/Scripts/SomeClass.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SomeClass : MonoBehaviour
+{
+    void Start()
+    {
+        List<BadGuy> badguys = new List<BadGuy>();
+
+        badguys.Add(new BadGuy("PaladinoAteu", 20));
+        badguys.Add(new BadGuy("Bob", 1));
+        badguys.Add(new BadGuy("Anna", 3));
+        badguys.Add(new BadGuy("Gabriella", 5));
+        badguys.Add(new BadGuy("Gengar", 7));
+        badguys.Add(new BadGuy("BadEgg", 10));
+        badguys.Add(new BadGuy("AqrueiroCego", 15));
+        badguys.Add(new BadGuy("BardoUtil", 100));
+        badguys.Add(new BadGuy("Charmander", 15));
+        badguys.Add(new BadGuy("DoraAventureira", 0));
+
+        badguys.Sort();
+
+        foreach (BadGuy guy in badguys)
+        {
+            print(guy.name + " " + guy.level);
+        }
+
+        badguys.Clear();
+    }
+}
